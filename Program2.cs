7a30c8d5a126348421d603513e2b0d5f3d995e18
awanayyan123-star <awanayyan123@gmail.com>
2025-12-30 04:21:13 -0800

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Vehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Vehicle is starting");
+    }
+}
+class Car : Vehicle
+{
+    public void Drive()
+    {
+        Console.WriteLine("Car is driving");
+    }
+}
+class SportsCar : Car
+{
+    public void Speed()
+    {
+        Console.WriteLine("Sports car is very fast");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SportsCar sc = new SportsCar();
+
+        sc.Start();  
+        sc.Drive();   
+        sc.Speed();   
+
+        Console.WriteLine("Inheritance example completed");
+        Console.ReadLine();
+    }
+}
