eeab51b7b631ddd3732b342a535a6776c8c2d1e3
awanayyan123-star <awanayyan123@gmail.com>
2025-12-30 04:23:55 -0800

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Person
+{
+    public string name;
+    public int age;
+
+    public void ShowPerson()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+    }
+}
+class Student : Person
+{
+    public int rollNo;
+
+    public void ShowStudent()
+    {
+        Console.WriteLine("Roll Number: " + rollNo);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+
+        s1.name = "Ali";
+        s1.age = 20;
+        s1.rollNo = 101;
+
+        s1.ShowPerson();
+        s1.ShowStudent();
+
+        Console.ReadLine();
+    }
+}
