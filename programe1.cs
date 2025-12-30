f4ac488e5d19dc66f86778c908fb4b9512c1d63e
awanayyan123-star <awanayyan123@gmail.com>
2025-12-30 04:20:01 -0800

    Add files via upload

---
+++ programe1.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog d1 = new Dog();
+        d1.Eat();   // Parent class method
+        d1.Bark();  // Child class method
+
+        Console.WriteLine("Program finished");
+        Console.ReadLine();
+    }
+}
